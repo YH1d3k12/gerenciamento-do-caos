b2fc7ff8ecaa9995354de019cfae538af1bac3e6
I4g0m1t0 <iagorechtramontin@gmail.com>
2025-11-05 21:05:47 -0300

    Fix: Correção na função de envio de email

--- Projeto/js/email.js
+++ Projeto/js/email.js
@@ -2,5 +2,19 @@ function enviarContato() {
   var email = document.getElementById("email").value;
   if (email.indexOf("@") == -1) {
     alert("email inválido");
+  } else {
+    email = document.getElementById("email").value;
+    var nome = document.getElementById("nome").value;
+    var mensagem = document.getElementById("mensagem").value;
+    var dados = {
+      nome: nome,
+      email: email,
+      mensagem: mensagem,
+    };
+    fetch("/send.php", { method: "POST", body: JSON.stringify(dados) })
+      .then((r) => r.text())
+      .then((t) => {
+        alert("Mensagem enviada com sucesso!");
+      });
   }
 }
